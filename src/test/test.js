5deeb8e22297bec4a86a7979b6400d5578f2715b
陈桐 <tong.chen@geekplus.com>
2025-02-12 16:45:13 +0800

    badcode

---
+++ src/test/test.js
@@ -0,0 +1,13 @@
+// 假设有以下变量命名不符合驼峰式
+var user_name = "John Doe";
+var age_of_user = 30;
+var user_is_active = true;
+
+console.log("User Name: " + user_name);
+console.log("Age of User: " + age_of_user);
+console.log("User Is Active: " + user_is_active);
+
+var ABCD ="12343"
+var keycode = 1;
+
+var username="wwwsds";
